cf72011a29e9daf4716abc1d3a34e7e8e74c489b
Amit Bora <amitbora007@gmail.com>
2020-01-14 23:11:37 +0530

    main file

---
+++ locker.py
@@ -0,0 +1,103 @@
+import sqlite3, sys, os
+#from pyfiglet import Figlet   #use this to generate different figlets on local system and copy them from terminal or visit http://patorjk.com/software/taag/#p=display&f=Rectangles&t=PLOCKER%0A
+import colorama
+from colorama import Fore, Back, Style
+from getpass import getpass
+#standar,starwars,stop,tubular,twin-cob,univers,block,avatar,arrows,,sansi,sblood,script,shadow,slant,slscript,smshadow,speed,rozzo
+#text = Figlet(font='shadow') #jazmine,lean,marquee,npn_____,o8,peaks,pawp,pebbles,pepper,poison,puffy,radical_,rectangles,roman,rounded
+#print(text.renderText('PLocker'))
+width = os.get_terminal_size().columns
+print(width)
+colorama.init()
+text='''                                       
+
+              _ \  |                |
+             |   | |      _ \   __| |  /  _ \  __|
+             ___/  |     (   | (      <   __/ |
+            _|    _____|\___/ \___|_|\_\\___|_|                                        
+
+'''
+print(Fore.GREEN + text)                                          
+                                         
+
+conn = sqlite3.connect("locker.db")
+table =  conn.execute("CREATE TABLE IF NOT EXISTS plocker(ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, SITE TEXT NOT NULL, USER TEXT NOT NULL, PWD TEXT NOT NULL)")
+#if table:
+#	print("Table created successfully.".center(width))
+	
+def insert_():
+	site = input("enter sitename: ")
+	user = input("enter Username: ")
+	pwd = input("enter password: ")
+
+	query = "INSERT INTO plocker(SITE, USER, PWD) VALUES(?,?,?)"
+	value = (f'{site}',f'{user}',f'{pwd}')
+	store = conn.execute(query,value)
+	conn.commit()
+	if(store):
+		print(Fore.yellow+"Data saved".center(width))
+	print("\n")
+	menu()
+	
+def retrieve_():	
+	retrieve = conn.execute("Select * from plocker")
+	for i in retrieve:
+		print(i.center(width))
+	print("\n")
+	menu()
+	
+def delete_():	
+	value = int(input("Enter ID of user you want to delete: "))
+	query = "delete from plocker where ID=?"
+	values = f'{value}'
+	delete = conn.execute(query,values)
+	conn.commit()
+	if(delete):
+		print(Fore.yellow+f"ID {value} removed".center(width))
+	print("\n")	
+	menu()
+
+def update_():
+	value = int(input("Enter ID of user you want to update: "))
+	npass = input("Enter New Password: ".center(width))
+	query = "UPDATE plocker set PWD=? where ID=?"
+	values = (f'{npass}',f'{value}')
+	update = conn.execute(query,values)
+	conn.commit()
+	if(update):
+		print(Fore.yellow +f"ID {value} updated".center(width))
+	print("\n")	
+	menu()
+	
+def menu():
+	print('''1. Insert data		2. Retrieve data
+3. Delete data 		4. Update Password
+5. Exit \n''')	
+	opt = int(input("Enter choice: "))
+	if(opt==1):
+		insert_()
+	elif(opt==2):
+		retrieve_()
+	elif(opt==3):
+		delete_()
+	elif(opt==4): 			
+		update_()
+	elif(opt==5):
+		sys.exit()
+	else: 
+		print(Fore.RED+"Wrong input provided!!!".center(width))
+	
+if __name__=="__main__":	
+	luser, lpass= "admin", "mypwdlocker"
+	
+	uname = input("enter login username: ")
+	upass = getpass("enter login password: ") 
+	
+	if((uname==luser) and (upass==lpass)):
+		print("Welcome to locker\n".center(width))
+		menu()				
+	else:
+		print(Fore.RED+"Incorrect Credentials!!!".center(width))
+        
+        
+        
